daab51d565e15a7d3393c7387ce8356549cb06c1
Md-Mahbub-Hasan <trylife.mahbub@gmail.com>
2021-09-14 21:09:54 -0700

    project setup and navbar design done

--- components/ShareComponents/Navbar.js
+++ components/ShareComponents/Navbar.js
@@ -1,10 +1,11 @@
 /* eslint-disable @next/next/no-img-element */
 /* eslint-disable @next/next/no-html-link-for-pages */
 import React from 'react';
+import Link from 'next/link';
 
 const Navbar = () => {
     return (
-        <div>
+        <div id="c_s_Navbar_area">
             {/* <!-- Navbar --> */}
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 {/* <!-- Container wrapper --> */}
@@ -12,11 +13,11 @@ const Navbar = () => {
                     {/* <!-- Navbar brand --> */}
                     <a className="navbar-brand me-2" href="/">
                         <img
-                            src="https://mdbootstrap.com/img/logo/mdb-transaprent-noshadows.png"
-                            height="16"
+                            src="/Assets/Illustration/PNG/Sahara Logo.png"
+                            height="40"
                             alt=""
                             loading="lazy"
-                            style={{marginTop: "-1px"}}
+                            style={{ marginTop: "-1px" }}
                         />
                     </a>
 
@@ -38,24 +39,38 @@ const Navbar = () => {
                         {/* <!-- Left links --> */}
                         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link" href="#">Dashboard</a>
+                                <Link href="#">
+                                    <a className="nav-link">Gabung kami</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="#">
+                                    <a className="nav-link">Blog</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="#">
+                                    <a className="nav-link">Tentang Kami</a>
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link href="#">
+                                    <a className="nav-link">Karir</a>
+                                </Link>
                             </li>
                         </ul>
                         {/* <!-- Left links --> */}
 
                         <div className="d-flex align-items-center">
-                            <button type="button" className="btn btn-link px-3 me-2">
-                                Login
+                            <button type="button" className="btn btn-primary me-3 def_active_btn">
+                                Belanja Sekarang
                             </button>
-                            <button type="button" className="btn btn-primary me-3">
-                                Sign up for free
+                            <button type="button" className="btn btn-link px-3 btn-ID">
+                                ID
                             </button>
-                            <a
-                                className="btn btn-dark px-3"
-                                href="https://github.com/mdbootstrap/mdb-ui-kit"
-                                role="button"
-                            ><i className="fab fa-github"></i
-                            ></a>
+                            <button type="button" className="btn btn-link px-3 btn-EN">
+                                EN
+                            </button>
                         </div>
                     </div>
                     {/* <!-- Collapsible wrapper --> */}
